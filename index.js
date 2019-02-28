29684b0bdba2cb2f93646a124b69c94e4f89f58f
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 17:04:40 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-// add solution here
-
-function theBeatlesPlay (musicians, instruments) {
-  var arr =[];
-  for (let i=0, l=musicians.length; i<l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}.`);
-    }
-  return arr;
-}

6829c6a6981a71258bf4bcb365bcecff253a95db
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 17:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function johnLennonFacts (facts) {
   var arr = [];
   let i=0;
   while (i<facts.length) {
-    arr.push(`${facts}!!!`);
+    arr.push(`${facts[i]}!!!`);
     i++;
   }
   return arr;
